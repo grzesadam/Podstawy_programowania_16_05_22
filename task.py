696cb68da70cfd4b6553f01532dbfeddfc7a2022
MagdalenaMorus <magdalenamor00@gmail.com>
2022-05-23 12:07:02 +0200

    add matrix and plots code

--- task.py
+++ task.py
@@ -1,17 +1,30 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+a = 2
+b = 123
+N = 1000
+M = 100
+K = 10
+
 
 def matrix():
-    a = 2
-    b = 123
-    N = 100
-    M = 100
-    K = 10
-    row = []
+    hists = []
     bins = np.linspace(a, b, K + 1)
     for i in range(M):
         data = np.random.uniform(a, b, N)
         hist = np.histogram(data, bins)[0]
-        row.append(hist)
-    print(np.array(row).shape)
-matrix()
+        hists.append(hist)
+    return hists, bins
+
+
+def plots():
+    array = np.array(hist_data)
+    for i in range(K):
+        hist, bins = np.histogram(array[:,i])
+        plt.bar(bins[:-1], hist)
+        plt.show()
+
+
+hist_data, bins_data = matrix()
+plots()

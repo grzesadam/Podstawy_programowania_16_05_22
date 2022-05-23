6b4149abd9b9beee707825021870460552645d7f
bb301974 <bb301974@student.polsl.pl>
2022-05-23 16:14:50 +0200

    Ostateczna wersja

--- RozkłaD LICZBY WARTOŚCI w przedziale klasowym.py
+++ RozkłaD LICZBY WARTOŚCI w przedziale klasowym.py
@@ -2,9 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-
 rng = np.random.default_rng()
-n = 10000
+n = 100
 # number of numbers
 m = 100
 # number of function calls
@@ -12,7 +11,9 @@ a = -10
 # edge min
 b = 10
 # edge max
-k = 21
+k = 20
+
+
 # number of class_interval
 
 
@@ -42,25 +43,36 @@ def matrix(m):
     return s
 
 
-h=matrix(m)
+h = matrix(m)
 s = matrix(m)
 
-co = math.ceil((k - 1) / 4)
+co = int((k - 1) / 4)
+print(co)
+
+
 # number of rows
-def historams(co,s):
 
-    fig, ax = plt.subplots(nrows=co, ncols=4, sharex='col', sharey='row', figsize=(7, 7), dpi=300, squeeze=True)
+
+def historams(co, s):
+    fig, ax = plt.subplots(nrows=co, ncols=4, sharex='col', sharey='row', figsize=(10, 10), dpi=300, squeeze=True)
     fig.suptitle('Histogram of the values for each column')
-
     ax = ax.ravel()
-
+    bins1 = np.linspace(np.min(s), np.max(s), k)
+    for x in bins1:
+        print(x, end=' ')
     for idx, axi in enumerate(ax):
-        bins1 =np.linspace(0,max(s[idx]),len([i for i in s[idx] if i!=1]))
-        axi.hist(s[idx],bins=bins1)
+        print(bins1[idx])
+        average = (max(s[idx]) + min(s[idx])) / 2
+        standard_deviation = round((max(s[idx]) - min(s[idx])) / (2 * math.sqrt(3)), 1)
+
+        labels = f'stdev={standard_deviation}\n av ={average}\n bin={round(bins1[idx],1)}'
+
+        axi.hist(s[idx], bins=bins1, label=labels)
+        axi.legend(loc='upper left')
         plt.xlabel('number')
         plt.ylabel('numbers of number')
 
-    plt.tight_layout()
     plt.show()
-historams(co,s)
-print(h)
+
+
+historams(co, s)

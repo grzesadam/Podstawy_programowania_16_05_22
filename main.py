302b2568ffc722ce4c6d74ebb2907e1b678a9ef6
unknown <sz302054@student.polsl.pl>
2022-05-29 22:56:20 +0200

    Prawie gotowe poza przedziałem klacowym

--- main.py
+++ main.py
@@ -8,30 +8,51 @@ rng = np.random.default_rng()
 # [a, b] = przedział liczb losowych
 # K = wartości przedziałów klasowych (bins), według których tworzony będzie histogram
 
-N = 100  # ilosć liczb losowych
+N = 10000  # ilosć liczb losowych
 A = 0  # początek przedizału
 B = 1  # koniec przedziału
-K = 10  # liczba kolumn
-M = 10  # liczba wierszy
+K = 20  # liczba kolumn
+M = 10000  # liczba wierszy
 
 
 def polecenie_3(m):
     bins = np.linspace(A, B, K + 1, endpoint=True)
-    for i in range(m):
-        if i == 0:
-            generated = rng.uniform(low=A, high=B, size=N)
-            result, _ = np.histogram(generated, bins)
-        else:
-            generated = rng.uniform(low=A, high=B, size=N)
-            histogram, _ = np.histogram(generated, bins)
-            result = np.vstack([result, histogram])
+    generated = rng.uniform(low=A, high=B, size=N)
+    result, _ = np.histogram(generated, bins)
+    for _ in range(m - 1):
+        generated = rng.uniform(low=A, high=B, size=N)
+        histogram, _ = np.histogram(generated, bins)
+        result = np.vstack([result, histogram])
 
     return result
 
 matrix = polecenie_3(M)
 
-fig, axs = plt.subplots(1, K)
-for i in range(K):
-    axs[i].hist(matrix[i, :])
+# print(matrix)
+
+histograms_array = matrix.T
+# histograms_array = matrix
+
+# print(histograms_array)
+
+
+# print((K - 1) // 4 + 1)
+
+if K > 4:
+    fig, axs = plt.subplots((K - 1) // 4 + 1, 4)
+
+    for i in range(K):
+        x = (i - 1) // 4
+        y = i % 4
+        label = f"Deviation = {np.std(histograms_array[i])}\nAverage = {np.average(histograms_array[i])}"
+        axs[x, y].hist(histograms_array[i], label=label)
+        axs[x, y].legend(loc='upper left', fontsize=7)
+else:
+    fig, axs = plt.subplots(1, 4)
+
+    for i in range(K):
+        label = f"Deviation = {np.std(histograms_array[i])}\nAverage = {np.average(histograms_array[i])}"
+        axs[i].hist(histograms_array[i], label=label)
+        axs[i].legend(loc='upper left', fontsize=7)
 
 plt.show()
